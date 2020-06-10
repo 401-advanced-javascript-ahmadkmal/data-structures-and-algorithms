ad59d7c2a030f177c8d93501d9d570d3fb1d66f2
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-06-10 17:30:02 +0300

    add eslint and fix it , add bracket challenge

--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -2,102 +2,103 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.previous = null;
+    this.next = null;
   }
 }
 class LinkedList {
-    constructor() {
-      this.head = null;
-    }
-    insert(value) {
-        const node = new Node(value);
-        if (!this.head) {
-          this.head = node;
-          return this;
-        }
-        node.previous=this.head
-        this.head = node;
-        return this;
-    }
-    includes(value) {
-        let currentNode = this.head;
-        if(currentNode.value===value)
-            return true;
-        while (currentNode.previous) {
-          currentNode = currentNode.previous;
-          if(currentNode.value===value)
-            return true;
-        }
-        return false;
-    }
-    toString(){
-        let currentNode = this.head;
-        let str = `{${currentNode.value}}`;
-        while (currentNode.previous) {
-          currentNode = currentNode.previous;
-          str += `->${currentNode.value}`;
-        }
-        return str;
-    }
-    append(value) {
-      const node = new Node(value);
-      if (!this.head) {
-        this.head = node;
-        return this;
-      }
-      let currentNode = this.head;
-      while (currentNode.previous) {
-        currentNode = currentNode.previous;
-      }
-      currentNode.previous = node;
+  constructor() {
+    this.head = null;
+  }
+  insert(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
       return this;
     }
-    insertBefore(value, newVal){
-      const node = new Node(newVal);
-      let currentNode = this.head;
-      if(currentNode.value===value){
-        node.previous=this.head;
-        this.head=node;
-        return
-      }
-      while(currentNode.previous.value!==value){
-        if(!currentNode.previous.previous)
-          return 
-        currentNode=currentNode.previous;
+    node.next=this.head;
+    this.head = node;
+    return this;
+  }
+  includes(value) {
+    let currentNode = this.head;
+    if(currentNode.value===value)
+      return true;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      if(currentNode.value===value)
+        return true;
+    }
+    return false;
+  }
+  toString(){
+    let currentNode = this.head;
+    let str = `{${currentNode.value}}`;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      str += `->${currentNode.value}`;
+    }
+    return str;
+  }
+  append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+  insertBefore(value, newVal){
+    const node = new Node(newVal);
+    let currentNode = this.head;
+    if(currentNode.value===value){
+      node.next=this.head;
+      this.head=node;
+      return;
+    }
+    while(currentNode.next.value!==value){
+      if(!currentNode.next.next)
+        return; 
+      currentNode=currentNode.next;
       
-      }
-      node.previous=currentNode.previous;
-      currentNode.previous=node;
-      return
     }
-    insertAfter(value, newVal){
-      const node = new Node(newVal);
-      let currentNode = this.head;
-      while(currentNode.value!==value){
-        if(!currentNode.previous)
-          return 
-        currentNode=currentNode.previous;
+    node.next=currentNode.next;
+    currentNode.next=node;
+    return;
+  }
+  insertAfter(value, newVal){
+    const node = new Node(newVal);
+    let currentNode = this.head;
+    while(currentNode.value!==value){
+      if(!currentNode.next)
+        return; 
+      currentNode=currentNode.next;
         
-      }
-      node.previous=currentNode.previous;
-      currentNode.previous=node;
-      return
     }
-    kthFromEnd(k){
-      let cunt =0;
-      while (currentNode.previous) {
-        currentNode = currentNode.previous;
-        cunt++;
-      }
-      if(cunt<k)
-        return false;
+    node.next=currentNode.next;
+    currentNode.next=node;
+    return;
+  }
+  kthFromEnd(k){
+    let cunt =0;
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      cunt++;
+    }
+    if(cunt<k)
+      return false;
 
-      while (cunt-k) {
-        currentNode = currentNode.previous;
-        cunt--;
-      }
-      return currentNode.value
+    while (cunt-k) {
+      currentNode = currentNode.next;
+      cunt--;
     }
+    return currentNode.value;
+  }
 
 
 }
